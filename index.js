dcfa7d6ce01bfe535f0910543314e8b32fa4c21e
Margarita_Pavlova <rito4ka298@yandex.ru>
2024-04-02 11:59:30 +0300

    finish

--- index.js
+++ index.js
@@ -1,22 +1,60 @@
-$(document).ready(function() {
-    $('.single-product__gallery_slider').slick({
-      infinite: false, // бесконечная прокрутка слайдов
-      slidesToShow: 2, // показывать по 2 слайда
-      slidesToScroll: 1, // прокручивать по 1 слайду за раз
-      responsive: [{
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
-      ]
-    });
+jQuery(function() {
+
+  //Ставка
+  jQuery("#slider-range-max").slider({
+    range: "max",
+    min: 0.5,
+    max: 15,
+    value: 5,
+    step: 0.5,
+    slide: function(event, ui) {
+      jQuery("#amount").val(ui.value);
+      calc();
+    }
   });
+  jQuery("#amount").val(jQuery("#slider-range-max").slider("value"));
+});
+jQuery(function() {
+  //Первоначальный взнос
+  price = document.getElementById("mortgage-calculator__price-value").innerHTML;
+  initialFeeFirst = price * 0.1;
+  jQuery("#slider-range-max2").slider({
+    range: "max",
+    min: 0,
+    max: price,
+    step: 10000,
+    value: initialFeeFirst,
+    slide: function(event, ui) {
+      jQuery("#amount2").val(ui.value);
+      calc();
+    }
+  });
+  jQuery("#amount2").val(jQuery("#slider-range-max2").slider("value"));
+});
+jQuery(function() {
+  // Срок кредита, лет
+  jQuery("#slider-range-max3").slider({
+    range: "max",
+    min: 1,
+    max: 40,
+    value: 30,
+    step: 1,
+    slide: function(event, ui) {
+      jQuery("#amount3").val(ui.value);
+      calc();
+    }
+  });
+  jQuery("#amount3").val(jQuery("#slider-range-max3").slider("value"));
+});
+
+function calc(par) {
+
+  price = document.getElementById("mortgage-calculator__price-value").innerHTML;
+  creditTerm = document.cl_form.amount3.value * 12;
+  rate = document.cl_form.amount.value;
+  rateMonth = rate / creditTerm / 100;
+  initialFee = document.cl_form.amount2.value;
+  paymentValue = Math.round(((price - initialFee)) * rateMonth * ((1 + rateMonth) ** creditTerm) / (((1 + rateMonth) ** creditTerm) - 1));
+  document.cl_form.summFinal.value = paymentValue;
+  return false;
+}
